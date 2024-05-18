c79d087a408ee5998392c908b52b7ab419251ce2
nasyaoy <122662286+nasyaoy@users.noreply.github.com>
2024-05-18 14:50:05 +0500

    менеджер финансов

---
+++ main.js
@@ -0,0 +1,41 @@
+const bnt = document.querySelector('.btn');
+const inputName = document.getElementById('name');
+const inputPrice = document.getElementById('price');
+const selectCategory = document.getElementById('selectCategory');
+const purcasheList = document.getElementById('tbody');
+
+bnt.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newLine = document.createElement('tr');
+    newLine.classList.add('purcashes__item');
+    newLine.classList.add('purcashes__row');
+
+    const newName = document.createElement('td');
+    const newCategory = document.createElement('td');
+    const newPrice = document.createElement('td');
+    newName.classList.add('purcashes__td');
+    newCategory.classList.add('purcashes__td');
+    newPrice.classList.add('purcashes__td');
+
+    newLine.appendChild(newName);
+    newLine.appendChild(newCategory);
+    newLine.appendChild(newPrice);
+    purcasheList.appendChild(newLine);
+
+    newName.textContent = inputName.value;
+    inputName.value = ' ';
+
+    newCategory.textContent = selectCategory.value;
+    newCategory.value = ' ';
+
+    newPrice.textContent = inputPrice.value;
+    inputPrice.value = ' ';
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = '×';
+    newLine.appendChild(delBtn);
+
+    delBtn.addEventListener('click', () => {
+        purcasheList.removeChild(newLine);
+    });
+})
